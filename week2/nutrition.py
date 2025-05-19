fc432620792fa21758a8373c9b7b3d0dd875daca
khlabib <193776169+khlabib@users.noreply.github.com>
2025-05-19 20:09:45 +0600

    solved the nutrition problem

---
+++ week2/nutrition.py
@@ -0,0 +1,45 @@
+item=input('Item: ').lower()
+#print('Calories: ',end='')
+match item:
+    case 'apple':
+        print('Calories: 130')
+    case 'avocado':
+        print('Calories: 50')
+    case 'banana':
+        print('Calories: 110')
+    case 'cantaloupe':
+        print('Calories: 50')
+    case 'grapefruit':
+        print('Calories: 60')
+    case 'grapes':
+        print('Calories: 90')
+    case 'honeydrew Melon':
+        print('Calories: 50')
+    case 'kiwifruit':
+        print('Calories: 90')
+    case 'lemon':
+        print('Calories: 15')
+    case 'lime':
+        print('Calories: 20')
+    case 'nectarine':
+        print('Calories: 60')
+    case 'orange':
+        print('Calories: 80')
+    case 'peach':
+        print('Calories: 60')
+    case 'pear':
+        print('Calories: 100')
+    case 'pineapple':
+        print('Calories: 50')
+    case 'plums':
+        print('Calories: 70')
+    case 'strawberries':
+        print('Calories: 50')
+    case 'sweet cherries':
+        print('Calories: 100')
+    case 'tangerine':
+        print('Calories: 50')
+    case 'watermeon':
+        print('Calories: 80')
+    case _:
+        pass
